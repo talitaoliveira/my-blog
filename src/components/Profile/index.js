f5060d6bcaa20556ed39245c179ed43509afc807
Talita Oliveira <litaa.oliveira@gmail.com>
2019-12-22 10:49:44 -0300

    Profile Page

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Profile = () => {
+	return (
+		<div className="profile-wrapper">
+			<h1>Talita Oliveira</h1>
+			<h1>Software Developer</h1>
+			<p>Tentando começar a escrever alguma coisa</p>
+		</div>
+	)
+
+}
+
+export default Profile;
