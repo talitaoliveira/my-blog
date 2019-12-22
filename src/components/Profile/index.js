4a15e61f737ee402a4b44f3287911d2df8cb1295
Talita Oliveira <litaa.oliveira@gmail.com>
2019-12-22 11:28:11 -0300

    Some changes on profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,31 +1,31 @@
 import React from 'react'
-import { StaticQuery, graphql } from gatsby
+import { StaticQuery, graphql } from 'gatsby'
 
-const Profile = () => {
-	return (
-		<StaticQuery
-			query={
-				graphql`
-				query MySiteMetadata {
-					site {
-						siteMetadata {
-							title
-							position
-							description
-						}
+const Profile = () => (
+  <StaticQuery
+    query={graphql`
+			query MySiteMetadata {
+				site {
+					siteMetadata {
+						title
+						position
+						description
 					}
 				}
-				`
 			}
-			render={data => {
-				<div className="profile-wrapper">
-					<h1>{data.site.siteMetadata.title}</h1>
-					<h1>{data.site.siteMetadata.position}</h1>
-					<p>{data.site.siteMetadata.description}</p>
-				</div>
-			}}
-		/>
-	)
-}
+		`}
+    render={({
+      site: {
+        siteMetadata: { title, position, description }
+      }
+    }) => (
+        <div className="profile-wrapper">
+          <h1>{title}</h1>
+          <h1>{position}</h1>
+          <p>{description}</p>
+        </div>
+      )}
+  />
+)
 
 export default Profile;
