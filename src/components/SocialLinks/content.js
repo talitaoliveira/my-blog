6dc17c2de231334abb89c49fa1b179a9caa9d29b
Talita Oliveira <litaa.oliveira@gmail.com>
2019-12-24 15:10:53 -0300

    Create a component for icons imported from styled icons and a file with the links

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,16 @@
+const links = [
+    {
+        label: 'Github',
+        url: 'https://github.com/talitaoliveira'
+    },
+    {
+        label: 'Twitter',
+        url: 'https://twitter.com/liitacherry'
+    },
+    {
+        label: 'Instagram',
+        url: 'https://instagram.com/liitacherry'
+    }
+]
+
+export default links
