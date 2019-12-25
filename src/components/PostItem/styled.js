0a4d7b4ea5c29e8dc7ec2325f1211fe566c9e770
Talita Oliveira <litaa.oliveira@gmail.com>
2019-12-25 18:49:29 -0300

    Create component PostItem and styled components for the structure

---
+++ src/components/PostItem/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+import {Link} from 'gatsby'
+
+export const PostItemLink = styled(Link)``
+
+export const PostItemWrapper = styled.section``
+
+export const PostItemTag = styled.div``
+
+export const PostItemInfo = styled.div``
+
+export const PostItemDate = styled.time``
+
+export const PostItemTitle = styled.h1``
+
+export const PostItemDescription = styled.p``
+
+
