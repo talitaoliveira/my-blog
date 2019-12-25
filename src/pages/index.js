f49acff82a66f17aee6dfbaaaea31ef18728f410
Talita Oliveira <litaa.oliveira@gmail.com>
2019-12-25 19:28:48 -0300

    Sending props to PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,18 @@ import SEO from "../components/seo"
 import PostItem from '../components/PostItem'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem/>
-  </Layout>
+    <Layout>
+        <SEO title="Home" />
+        <PostItem
+            slug = "/slug/"
+            background = "red"
+            category = "Misc"
+            date = "25 de Dezembro de 2019"
+            timeToRead = "4"
+            title = "Seja bem vinda ao meu blog"
+            description = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum vitae soluta ipsam necessitatibus esse voluptate in corporis iure nulla repellat saepe nobis, sed rerum possimus, quidem nam fugiat quae nihil!" />
+    </Layout>
 )
+
 
 export default IndexPage
