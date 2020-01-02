aadcff8c295244fafd3e2a5550758c26f57f40ac
Talita Oliveira <litaa.oliveira@gmail.com>
2020-01-02 19:13:42 -0300

    Style blog-post on small screen

--- src/components/Post/styled.js
+++ src/components/Post/styled.js
@@ -1,10 +1,15 @@
 import styled from "styled-components"
+import media from 'styled-media-query'
 
 export const PostHeader = styled.header`
   color: var(--postColor);
   margin: auto;
   max-width: 70rem;
   padding: 5rem 5rem 0;
+
+  ${media.lessThan("large")`
+    padding: 2rem;
+  `}
 `
 
 export const PostTitle = styled.h1`
@@ -12,18 +17,33 @@ export const PostTitle = styled.h1`
   font-weight: 700;
   padding: 0 1.4rem;
   margin: 1rem auto;
+
+  ${media.lessThan("large")`
+    padding: 0;
+    font-size: 2.2rem;
+  `}
 `
 
 export const PostDescription = styled.h2`
   font-size: 2rem;
   font-weight: 200;
   padding: 0 1.4rem;
+
+  ${media.lessThan("large")`
+    padding: 0;
+    font-size: 1.2rem;
+  `}
 `
 
 export const PostDate = styled.p`
   font-size: 1.1rem;
   font-weight: 100;
   padding: 0 1.4rem;
+
+  ${media.lessThan("large")`
+    padding: 0;
+    font-size: 1rem;
+  `}
 `
 
 export const MainContent = styled.section`
@@ -159,4 +179,8 @@ export const MainContent = styled.section`
       opacity: 0.8;
     }
   }
+
+  ${media.lessThan("large")`
+    padding: 0;
+  `}
 `
