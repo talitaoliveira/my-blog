b0aef338aa8d240511cad284266a78f0193d5cdf
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2020-01-06 15:43:06 -0300

    Edit manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,11 +60,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Talita Oliveira`,
+        short_name: `Talita Oliveira`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#16202c`,
+        theme_color: `#16202c`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
