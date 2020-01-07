2c64aa6bc43fa710c5d7cd1bc90546d10b3b0dfe
Talita Oliveira <latalita.s@Talitas-MacBook-Pro.local>
2020-01-07 10:42:27 -0300

    Add plugin netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-sitemap`,
+    `gatsby-plugin-netlify-cms`,
     // needs to be the first one to work with <gatsby-remar0></gatsby-remar0>
     {
         resolve: `gatsby-source-filesystem`,
