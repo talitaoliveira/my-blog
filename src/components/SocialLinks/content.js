5df52eff4d6f33501779920feef729239612ac15
Talita Oliveira <litaa.oliveira@gmail.com>
2020-05-10 21:51:09 -0300

    Add linkedin icon

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -10,6 +10,10 @@ const links = [
     {
         label: 'Instagram',
         url: 'https://instagram.com/liitacherry'
+    },
+    {
+        label: 'Linkedin',
+        url: 'https://www.linkedin.com/in/litaaoliveira/'
     }
 ]
 
