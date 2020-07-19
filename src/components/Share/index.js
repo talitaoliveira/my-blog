98cdfd8f3030ce476ae4ab5576f5e6274717ed51
Talita Oliveira <litaa.oliveira@gmail.com>
2020-07-18 22:55:55 -0300

    Remove unecessary code

--- src/components/Share/index.js
+++ src/components/Share/index.js
@@ -6,20 +6,16 @@ import ShareLinks from '../ShareLinks'
 import * as S from './styled';
 
 const hasShareNative = () => {
-    console.log('-----')
-    console.log(navigator.share)
     if (navigator.share) {
-        console.log('sim')
         return true
     }
-    console.log('nao')
     return false
   }
 
 const Share = () => (
     <S.ShareWrapper>
         <S.ShareDescription>Compartilhe:</S.ShareDescription>
-        {hasShareNative ? <ShareNative/> : <ShareLinks/>}
+        {hasShareNative() ? <ShareNative/> : <ShareLinks/>}
 
     </S.ShareWrapper>
 )
