86192efb34fe1d7ddfb28196b5c1286c95fb5137
Talita Oliveira <litaa.oliveira@gmail.com>
2020-08-11 20:47:13 -0300

    Improving social links

--- src/components/ShareLinks/content.js
+++ src/components/ShareLinks/content.js
@@ -1,15 +1,21 @@
 const links = [
     {
         label: 'Twitter',
-        url: 'http://twitter.com/share?text=Blog Talita Oliveira - &url=http://blog.talitaoliveira.com.br'
+        url: function (text, url) {
+            return `https://twitter.com/intent/tweet?text="${text}" - @liitacherry &url=https://blog.talitaoliveira.com.br${url}`
+        }
     },
     {
         label: 'Linkedin',
-        url: 'https://www.linkedin.com/sharing/share-offsite/?title=TalitaOliveira&url=http://blog.talitaoliveira.com.br'
+        url: function (text, url) {
+            return `https://www.linkedin.com/sharing/share-offsite/?&url=https://blog.talitaoliveira.com.br${url}`
+        }
     },
     {
         label: 'Facebook',
-        url: 'http://www.facebook.com/sharer.php?title=TalitaOliveira&u=http://blog.talitaoliveira.com.br'
+        url: function (text, url) {
+            return `http://www.facebook.com/sharer.php?&u=https://blog.talitaoliveira.com.br${url}`
+        }
     }
 ]
 
